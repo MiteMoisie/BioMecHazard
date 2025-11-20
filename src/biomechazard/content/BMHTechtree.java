50d6261fafe1a70243af72f32af52609a11626bd
MiteMoisie <kjb@hotmail.fr>
2025-11-20 07:14:18 +0100

    Add files via upload

---
+++ src/biomechazard/content/BMHTechtree.java
@@ -0,0 +1,18 @@
+package biomechazard.content;
+
+import arc.*;
+import arc.struct.*;
+import mindustry.content.SectorPresets;
+import mindustry.game.Objectives.*;
+
+import static mindustry.content.Blocks.*;
+import static mindustry.content.TechTree.*;
+import static mindustry.content.SectorPresets.*;
+
+public class BMHTechtree {
+    public static void load(){
+        nodeRoot("BioMecHazard", coreShard, false, () -> {
+			node(BMHBlocks.coreNethrax/*, Seq.with(new SectorComplete(biomassFacility))*/);
+		});
+	};
+};
