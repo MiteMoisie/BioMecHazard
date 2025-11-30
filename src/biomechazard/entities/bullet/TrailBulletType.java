8e8e29993b0491dd0eadc69a723668c8e6a4b9b6
MiteMoisie <kjb@hotmail.fr>
2025-11-30 18:15:47 +0100

    Add files via upload

---
+++ src/biomechazard/entities/bullet/TrailBulletType.java
@@ -0,0 +1,57 @@
+/**
+* @author Eschatologue 
+* mod "Unlimited Armament Works"
+*/
+
+package biomechazard.entities.bullet;
+
+import arc.math.Mathf;
+import mindustry.content.Fx;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.gen.Bullet;
+import biomechazard.audiovisual.*;
+
+/**
+ * BasicBulletType with automatic adjusting trails
+ */
+public class TrailBulletType extends BasicBulletType {
+	/**
+	 * How long is the generated trail based on height multiplied by this
+	 * <p> 0 to disable </p>
+	 */
+	public float trailLengthScale = 1f;
+	public float trailWidthScale = 0.289f;
+
+	public TrailBulletType(float speed, float damage, String bulletSprite) {
+		super(speed, damage);
+		this.sprite = bulletSprite;
+		trailEffect = Fx.none;
+	}
+
+	public TrailBulletType(float speed, float damage) {
+		this(speed, damage, "bullet");
+	}
+
+	public TrailBulletType() {
+		this(0f, 0f, "bullet");
+	}
+
+	@Override
+	public void init(Bullet b) {
+		super.init(b);
+		drawSize = Math.max(drawSize, trailLength * speed * 4.5f);
+		if (trailLengthScale > 0) {
+			trailRotation = true;
+			trailWidth = width * trailWidthScale;
+			trailLength = Mathf.round(height * trailLengthScale);
+			trailColor = backColor;
+		}
+	}
+
+	@Override
+	public void removed(Bullet b) {
+		if (trailLength > 0 && b.trail != null && b.trail.size() > 0) {
+			BMHEffects.trailFade.at(b.x, b.y, trailWidth, trailColor, b.trail.copy());
+		}
+	}
+}
