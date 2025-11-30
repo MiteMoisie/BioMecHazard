5d34f2ddbad8e71d4a51f97d53cb1128a657b7bd
MiteMoisie <kjb@hotmail.fr>
2025-11-30 17:36:25 +0100

    Update BMHEffects.java

--- src/biomechazard/audiovisual/BMHEffects.java
+++ src/biomechazard/audiovisual/BMHEffects.java
@@ -8,6 +8,7 @@ import mindustry.graphics.*;
 import static arc.graphics.g2d.Draw.*;
 import static arc.graphics.g2d.Lines.*;
 import static arc.math.Angles.*;
+import static biomechazard.audiovisual.UAWPal.*;
 
 public class BMHEffects {
 	public static Effect
@@ -35,8 +36,33 @@ public class BMHEffects {
 		randLenVectors(e.id, 3, 2f + e.fin() * 7f, (x, y) -> {
 			Fill.square(e.x + x, e.y + y, 0.2f + e.fslope() * 1.5f, 45f);
 		});
-	})
+	}),
 	// endregion Status
+	
+	/** @author Eschatologue  */
+	// region Trails
+	/**
+	 * Vanilla trail fade but with much larger clipsize
+	 */
+	trailFade = new Effect(400f, 400, e -> {
+		if (!(e.data instanceof Trail trail)) return;
+		//lifetime is how many frames it takes to fade out the trail
+		e.lifetime = trail.length * 1.4f;
+
+		if (!state.isPaused()) {
+			trail.shorten();
+		}
+		trail.drawCap(e.color, e.rotation);
+		trail.draw(e.color, e.rotation);
+	}),
+	
+	/** @author Eschatologue  */
+	/** Based on Fx.fireHit */
+	cryoHit = new Effect(38f, e -> {
+		Draw.color(cryoFront, cryoBack, e.fin());
+		Angles.randLenVectors(e.id, 5, 2f + e.fin() * 10f, (x, y) -> Fill.circle(e.x + x, e.y + y, 0.2f + e.fout() * 1.6f));
+		Draw.color();
+	}),
 	
 	;
 }
