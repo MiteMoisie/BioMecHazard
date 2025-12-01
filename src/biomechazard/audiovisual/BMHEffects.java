1957600a5a2279977463858dbb9e6895146b2606
MiteMoisie <kjb@hotmail.fr>
2025-12-01 12:57:37 +0100

    Update BMHEffects.java

--- src/biomechazard/audiovisual/BMHEffects.java
+++ src/biomechazard/audiovisual/BMHEffects.java
@@ -63,9 +63,59 @@ public class BMHEffects {
 		Draw.color(cryoFront, cryoBack, e.fin());
 		Angles.randLenVectors(e.id, 5, 2f + e.fin() * 10f, (x, y) -> Fill.circle(e.x + x, e.y + y, 0.2f + e.fout() * 1.6f));
 		Draw.color();
+	}),
+	
+	/** @author Eschatologue  */
+	// region Shooting
+	shootSmoke = new Effect(30f, e -> {
+		Draw.color(e.color, Color.lightGray, Color.gray, e.fin());
+
+		Angles.randLenVectors(e.id, 9, e.finpow() * 23f, e.rotation, 20f, (x, y) -> {
+			Fill.circle(e.x + x, e.y + y, e.fout() * 2.4f + 0.2f);
+		});
 	})
 	
 	;
+
+	/** @author Eschatologue  */
+	/** Refer to {@link UAWFx#shootSmoke(float, Color, boolean, float, float)} */
+	public static Effect shootSmoke(float lifetime, Color color) {
+		return shootSmoke(lifetime, color, false);
+	}
+	/** Refer to {@link UAWFx#shootSmoke(float, Color, boolean, float, float)} */
+	public static Effect shootSmoke(float lifetime, Color color, boolean muzzleBreak) {
+		return shootSmoke(lifetime, color, muzzleBreak, 1, 1);
+	}
+	/**
+	 * Modified version of {@link Fx#shootBigSmoke2}
+	 * @param lifetime
+	 * 	[18] How long does the smoke lasts, also adjusts amount, spreads, and radius.
+	 * @param color
+	 * 	The color of the beginning of the smoke, will lerp to gray
+	 * @param muzzleBreak
+	 * 	[False] Whenever to cause 2 instances of the effect and make it spread horizontally
+	 */
+	public static Effect shootSmoke(float lifetime, Color color, boolean muzzleBreak, float sizeMult, float lifetimeMult) {
+		int amount = (int) ((int) lifetime * 0.5f);
+		float l = lifetime * 1.27f;
+		float rng = lifetime * 1.1f;
+		float size = (lifetime * 0.13f) * sizeMult;
+		return new Effect(lifetime * lifetimeMult, e -> {
+			Draw.color(color, Color.lightGray, Color.gray, e.fin());
+
+			randLenVectors(e.id, amount, e.finpow() * l, e.rotation + (muzzleBreak ? 90 : 0), rng, (x, y) -> {
+				Fill.circle(e.x + x, e.y + y, e.fout() * size + (size * 0.011f));
+			});
+			if (muzzleBreak) {
+				randLenVectors(e.id, amount, e.finpow() * l, e.rotation + 270, rng, (x, y) -> {
+					Fill.circle(e.x + x, e.y + y, e.fout() * size + (size * 0.011f));
+				});
+			}
+			Draw.reset();
+		});
+	}
+	/** Fin du code UAW */
 }
+
 
 
