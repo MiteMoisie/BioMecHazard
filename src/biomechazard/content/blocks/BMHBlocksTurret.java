59068c1e170acafc574630d69ec4aa909a2dc10c
MiteMoisie <kjb@hotmail.fr>
2025-12-01 12:59:57 +0100

    Update BMHBlocksTurret.java

--- src/biomechazard/content/blocks/BMHBlocksTurret.java
+++ src/biomechazard/content/blocks/BMHBlocksTurret.java
@@ -151,6 +151,158 @@ public class BMHBlocksTurret {
 					}}
 				);
 			}};
+		}};;
+		
+		spitfire = new ItemTurret("spitfire") {{
+			requirements(turret, with(
+				lead, 350,
+				titanium, 280,
+				thorium, 250,
+				plastanium, 250,
+				silicon, 150
+			));
+			size = 3;
+			scaledHealth = 250;
+
+			reload = 2.5f;
+			recoil = 1f;
+			recoilTime = 60f;
+			maxAmmo = 200;
+			minWarmup = 0.95f;
+			shootWarmupSpeed = 0.025f;
+
+			range = 35 * tilesize;
+			inaccuracy = 7.5f;
+			rotateSpeed = 7f;
+			targetAir = false;
+
+			shootSound = Sounds.shootBig;
+
+			ammoUseEffect = casing2Double;
+
+			shoot = new ShootBarrel() {{
+				barrels = new float[]{
+					0f, 2f, 0f,
+					3f, 1f, 0f,
+					-3f, 1f, 0f,
+				};
+			}};
+
+			ammo(
+				graphite, new BasicBulletType(8, 20) {{
+					pierceCap = 2;
+					height = 22;
+					width = 10;
+					knockback = 1.2f;
+					hitEffect = hitBulletBig;
+					smokeEffect = shootBigSmoke2;
+					shootEffect = shootBig2;
+					trailChance = 0.4f;
+					trailColor = Color.lightGray;
+
+					status = StatusEffects.slow;
+					collidesAir = false;
+				}},
+				stoutsteel, new TrailBulletType(12, 22) {{
+					height = 20;
+					width = 5;
+					pierce = true;
+					pierceArmor = true;
+					despawnHit = true;
+					frontColor = titaniumFront;
+					backColor = titaniumBack;
+					hitEffect = new MultiEffect(hitBulletColor, generatespark);
+					smokeEffect = new MultiEffect(shootBigSmoke2, fireSmoke);
+					shootEffect = shootBigColor;
+					trailEffect = disperseTrail;
+					trailChance = 0.7f;
+					collidesAir = false;
+				}},
+				pyratite, new BasicBulletType(7, 15) {{
+					height = 25;
+					width = 10;
+					knockback = 1.2f;
+					hitEffect = hitBulletBig;
+					smokeEffect = shootBigSmoke2;
+					shootEffect = new MultiEffect(shootPyraFlame, shootBig2);
+					hitEffect = new MultiEffect(hitBulletBig, fireHit);
+					despawnEffect = fireHit;
+					frontColor = lightishOrange;
+					backColor = lightOrange;
+					status = StatusEffects.burning;
+					makeFire = true;
+					trailChance = 0.4f;
+					trailColor = Color.lightGray;
+					collidesAir = false;
+				}},
+				cryogel, new BasicBulletType(7, 15) {{
+					height = 25;
+					width = 10;
+					knockback = 1.2f;
+					hitEffect = hitBulletBig;
+					despawnEffect = freezing;
+					smokeEffect = shootBigSmoke2;
+					shootEffect = new MultiEffect(shootSmoke, shootBig2);
+					hitEffect = new MultiEffect(hitBulletBig, cryoHit);
+					despawnEffect = cryoHit;
+					frontColor = cryoFront;
+					backColor = cryoBack;
+					status = StatusEffects.freezing;
+					trailChance = 0.4f;
+					trailColor = Color.lightGray;
+					collidesAir = false;
+				}},
+				surgeAlloy, new BasicBulletType(8, 15) {{
+					height = 25;
+					width = 8;
+					hitEffect = despawnEffect = new MultiEffect(hitBulletBig, lightning);
+					smokeEffect = new MultiEffect(shootBigSmoke2, fireSmoke);
+					shootEffect = new MultiEffect(shootBig, sparkShoot);
+					status = StatusEffects.electrified;
+					trailChance = 0.4f;
+					trailColor = Color.lightGray;
+
+					lightning = 3;
+					lightningAngle = 360;
+					lightningDamage = 1.5f;
+					lightningLength = 12;
+					collidesAir = false;
+				}}
+			);
+			limitRange(2 * tilesize);
+
+			squareSprite = false;
+			cooldownTime = 2 * tick;
+			drawer = new DrawTurret(modTurretBase) {{
+				parts.addAll(
+					new RegionPart("-barrel") {{
+						heatProgress = PartProgress.heat.add(PartProgress.heat);
+						progress = PartProgress.warmup.add(PartProgress.reload.add(-2.5f));
+						moveY = 4 * px;
+					}},
+					new RegionPart("-side") {{
+						progress = PartProgress.warmup;
+						mirror = true;
+						moveX = 2f * px;
+						moveY = -4 * px;
+						moveRot = -22f;
+					}},
+					new RegionPart("-blade") {{
+						progress = PartProgress.warmup;
+						mirror = true;
+						moveX = -4 * px;
+						moveY = 8 * px;
+					}},
+					new RegionPart("-back") {{
+						progress = PartProgress.warmup;
+						moveY = -2.5f;
+					}},
+					new RegionPart("-top")
+				);
+			}};
 		}};
+
+		
 	}
 }
+
