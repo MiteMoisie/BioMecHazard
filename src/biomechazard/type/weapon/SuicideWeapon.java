9bdc6a89cdadeb76dfdb7b8bcdfded65b7c3a06b
MiteMoisie <kjb@hotmail.fr>
2025-12-03 02:21:42 +0100

    Add files via upload

---
+++ src/biomechazard/type/weapon/SuicideWeapon.java
@@ -0,0 +1,23 @@
+/**
+* @author Eschatologue 
+* mod "Unlimited Armament Works"
+*/
+
+package biomechazard.type.Weapon;
+
+/** Weapon that kills the user */
+public class SuicideWeapon extends Weapon {
+	public SuicideWeapon(String name) {
+		this.name = name;
+		shootCone = 360f;
+		shootOnDeath = true;
+		mirror = false;
+		reload = 1f;
+		shake = 5;
+	}
+
+	public SuicideWeapon() {
+		this("");
+	}
+
+}
