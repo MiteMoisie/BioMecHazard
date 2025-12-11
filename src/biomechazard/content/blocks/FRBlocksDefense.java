0a97c5da3aeb04a73b3f6361848d080866a78e0e
MiteMoisie <kjb@hotmail.fr>
2025-12-11 19:42:07 +0100

    Add files via upload

---
+++ src/biomechazard/content/blocks/FRBlocksDefense.java
@@ -0,0 +1,24 @@
+package biomechazard.content.blocks;
+
+import mindustry.content.*;
+import mindustry.world.*;
+import mindustry.world.blocks.defense.*;
+
+import biomechazard.content.*;
+
+public class FRBlocksDefense{
+	
+	public static Block plastaniumWallHuge;
+	
+	public static void load(){
+
+        plastaniumWallHuge = new Wall("plastanium-wall-2"){{
+            requirements(Category.defense, with(Items.plastanium, 54));
+            health = 4680;
+            size = 3;
+            insulated = true;
+            absorbLasers = true;
+        }};
+		
+	}
+}
