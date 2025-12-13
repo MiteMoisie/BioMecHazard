7957d3673b449c5dd79b6688ce2ec282502654b0
MiteMoisie <kjb@hotmail.fr>
2025-12-13 19:50:54 +0100

    Update BioMecHazardModLauncher.java

--- src/biomechazard/BioMecHazardModLauncher.java
+++ src/biomechazard/BioMecHazardModLauncher.java
@@ -121,10 +121,11 @@ public class BioMecHazardModLauncher extends Mod{
 		BMHTechtreeNodes.load();
 		BMHTechtree.load();
 		
-		Log.info("Chargement de BioMecHazard v0.0.18" /*+ MOD.meta.version*/ + " effectué | Durée écoulée : " + (Time.elapsed() / Time.toSeconds) + " sec.");
+		Log.info("Chargement de BioMecHazard v0.0.20" /*+ MOD.meta.version*/ + " effectué | Durée écoulée : " + (Time.elapsed() / Time.toSeconds) + " sec.");
     }
 
 }
+
 
 
 
