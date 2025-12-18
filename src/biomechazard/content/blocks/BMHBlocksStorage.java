715d94626af3bfd83879362eed8f3de48cba0e12
MiteMoisie <kjb@hotmail.fr>
2025-12-18 22:37:49 +0100

    Update BMHBlocksStorage.java

--- src/biomechazard/content/blocks/BMHBlocksStorage.java
+++ src/biomechazard/content/blocks/BMHBlocksStorage.java
@@ -33,6 +33,11 @@ public class BMHBlocksStorage{
 			liquidCapacity = 8000;
 			//liquidPressure = 800;
 			powerProduction = 14.5f;
+			drawer = new DrawMulti(
+				new DrawRegion("-bottom"), 
+				new DrawLiquidRegion(), 
+				new DrawDefault()
+			);
 		}};
 		
 		coreNexus = new CoreBlock("core-nexus"){{
@@ -114,6 +119,7 @@ public class BMHBlocksStorage{
 	}
 
 }
+
 
 
 
