66b846d639fca1fa6154a89df925f3416cfd19cd
MiteMoisie <kjb@hotmail.fr>
2025-12-23 22:55:54 +0100

    Add files via upload

---
+++ src/biomechazard/content/BMHSettings.java
@@ -0,0 +1,14 @@
+package biomechazard.content;
+
+public class BMHSettings{
+	
+	public static boolean enableEffectDetail = true;
+	
+	public static void load(){
+		
+	}
+	
+	public static boolean enableDetails(){
+		return enableEffectDetail;
+	}
+}
